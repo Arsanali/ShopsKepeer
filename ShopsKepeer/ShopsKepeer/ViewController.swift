defbc45f299f9d332fb25b342897a30acbe6a13c
Arslanali <arslanali.98@mail.ru>
2020-02-12 17:34:44 +0300

    new module

---
+++ ShopsKepeer/ShopsKepeer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopsKepeer
+//
+//  Created by arsik on 12.02.2020.
+//  Copyright © 2020 arsik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
