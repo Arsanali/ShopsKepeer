82c2063c0f819eff1f7b2b1db31c867ff4b38a21
Arslanali <arslanali.98@mail.ru>
2020-02-12 18:13:20 +0300

    created Interface

---
+++ ShopsKepeer/ShopsKepeer/Extension/Extension+String.swift
@@ -0,0 +1,9 @@
+//
+//  Extension+String.swift
+//  ShopsKepeer
+//
+//  Created by arsik on 12.02.2020.
+//  Copyright © 2020 arsik. All rights reserved.
+//
+
+import Foundation
---
+++ ShopsKepeer/ShopsKepeer/Extension/Extension+TextField.swift
@@ -0,0 +1,9 @@
+//
+//  Extension+TextField.swift
+//  ShopsKepeer
+//
+//  Created by arsik on 12.02.2020.
+//  Copyright © 2020 arsik. All rights reserved.
+//
+
+import Foundation
